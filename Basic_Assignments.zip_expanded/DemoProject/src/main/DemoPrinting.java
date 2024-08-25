586990d92113b1a697005e16c5ab6f74efaf82eb
sanket soanawane <154777847+Sanket2321@users.noreply.github.com>
2024-08-25 23:22:47 +0530

    Update DemoPrinting.java

--- Basic_Assignments.zip_expanded/DemoProject/src/main/DemoPrinting.java
+++ Basic_Assignments.zip_expanded/DemoProject/src/main/DemoPrinting.java
@@ -2,7 +2,7 @@ package main;
 
 public class DemoPrinting {
 	public static void main(String[] args) {
-		System.out.println("javabykiran");
+		System.out.println("java");
 		System.out.print("Karvenagar");
 		System.out.println(3);
 		System.out.println(3 + 4);
